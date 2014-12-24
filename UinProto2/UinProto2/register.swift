1cc5e2b7ef12fe2f8de78204890b7e895c58f475
Kareem Dasilva <TheRaven@Lancelots-iMac.local>
2014-12-24 10:16:09 -0600

    Caught up to progress yesterday

---
+++ UinProto2/UinProto2/register.swift
@@ -0,0 +1,83 @@
+//
+//  register.swift
+//  UinProto2
+//
+//  Created by Kareem Dasilva on 12/24/14.
+//  Copyright (c) 2014 Kareem Dasilva. All rights reserved.
+//
+
+import UIKit
+
+class register: UIViewController {
+
+    
+    @IBOutlet weak var username: UITextField!
+    
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
+    @IBOutlet weak var email: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func register(sender: AnyObject) {
+        
+        var error = ""
+        
+        if username.text == "" || password.text == "" {
+            
+            error = "You did not enter a username or password"
+            
+        }
+        if error == "" {
+            
+            var user = PFUser()
+            
+            user.username = username.text
+            
+            user.password = password.text
+            
+            user.email = email.text
+            
+            user.signUpInBackgroundWithBlock {
+                (succeeded: Bool!, registerError: NSError!) -> Void in
+                
+                if registerError == nil {
+                    
+                    println("register")
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
